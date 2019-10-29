06ba04f7ea3c0fc7ad3f6b205ab283c8c17eb183
fkwai <geofkwai@gmail.com>
2019-10-29 19:31:41 -0400

    regionalization add new cases

--- app/paperSigma/CONUS_regCombTemp.py
+++ app/paperSigma/CONUS_regCombTemp.py
@@ -6,7 +6,7 @@ import numpy as np
 import scipy
 import imp
 import statsmodels.api as sm
-from rnnSMAP import funPost
+from rnnSMAP.funPost import distCDF
 
 imp.reload(rnnSMAP)
 rnnSMAP.reload()
@@ -50,10 +50,10 @@ if 'loadData' in doOpt:
     valSigmaLst = list()
     valConfLst = list()
     modelLst = list()
-    testName = 'CONUSv2f1'
-    yr = [2016]
-    valName = 'CONUSv2f1'
-    valYr = [2017]
+    testName = 'CONUSv2fy2'
+    yr = [2015]
+    valName = 'CONUSv2fx2'
+    valYr = [2015]
 
     ds = rnnSMAP.classDB.DatasetPost(
         rootDB=rootDB, subsetName=testName, yrLst=yr)
@@ -85,8 +85,8 @@ labelLst = ['sigmaMC', 'sigmaX', 'sigmaComb'] +\
     ['sigmaReg opt '+str(x) for x in optLst]
 
 # calculate cdf distance
-testRmseLst, testKsdLst = funPost.distCDF(testConfLst)
-valRmseLst, valKsdLst = funPost.distCDF(valConfLst)
+testRmseLst, testKsdLst = distCDF(testConfLst)
+valRmseLst, valKsdLst = distCDF(valConfLst)
 
 for k in range(len(optLst)):
     wLst = modelLst[k].params.tolist()
@@ -108,6 +108,8 @@ for k in range(len(valSigmaLst)):
 for k in range(len(testSigmaLst)):
     print('test {}: SSR = {:.4f}, KS = {:.4f}'.format(
         labelLst[k], testSsrLst[k], testKsdLst[k]))
+
+
 
 
 #################################################
