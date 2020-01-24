ea1ea777d97ebeaa4a2d65ea6fcf467e0f26eb2c
fkwai <geofkwai@gmail.com>
2020-01-24 09:38:27 -0800

    fix bug sh

--- app/waterQual/model/modelC.py
+++ app/waterQual/model/modelC.py
@@ -19,7 +19,7 @@ nEpoch = 500
 saveEpoch = 100
 resumeEpoch = 0
 hiddenSize = 256
-modelFolder = os.path.join(kPath.dirWQ, 'modelA', caseName)
+modelFolder = os.path.join(kPath.dirWQ, 'modelC', caseName)
 if not os.path.exists(modelFolder):
     os.mkdir(modelFolder)
 
@@ -136,8 +136,8 @@ for iEp in range(resumeEpoch+1, nEpoch + 1):
                 [xNorm[:, iS[k]:iE[k], :], np.tile(cNorm[iS[k]:iE[k], :], [nt, 1, 1])], axis=-1)).float()
             if torch.cuda.is_available():
                 xT = xT.cuda()
-                modelTest = modelTest.cuda()
-            yT = modelTest(xT)[-1, :, :]
+                model = model.cuda()
+            yT = model(xT)[-1, :, :]
             yOutLst.append(yT.detach().cpu().numpy())
         temp = np.concatenate(yOutLst, axis=0)
         yOut = np.ndarray(temp.shape)
--- app/waterQual/model/remoteScript.py
+++ app/waterQual/model/remoteScript.py
@@ -1,3 +1,5 @@
 from hydroDL.master import slurm
 
-slurm.submitJobGPU('modelA','python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/modelB.py',nH=20)
+slurm.submitJobGPU('modelA','python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/modelA.py',nH=20)
+
+slurm.submitJobGPU('modelC','python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/modelC.py',nH=20)
