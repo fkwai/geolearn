28b1a406eb82875df26f965b7311225e5095d5ed
Kuai Fang <geofkwai@gmail.com>
2020-03-19 22:22:42 -0400

    Update basinRef.py

--- app/waterQual/model/basinRef.py
+++ app/waterQual/model/basinRef.py
@@ -6,7 +6,13 @@ wqData = waterQuality.DataModelWQ('basinRef')
 ind1 = wqData.indByRatio(0.8)
 ind2 = wqData.indByRatio(0.8, first=False)
 wqData.saveSubset(['first80', 'last20'], [ind1, ind2])
+
 caseName = basins.wrapMaster(
-    'basinRef', 'first80', batchSize=[None, 2000], optQ=1, outName='basinRef-opt1')
+    'basinRef', 'first80', batchSize=[None, 500], optQ=1, outName='basinRef-opt1')
+cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/cmdTrain.py -M'
+slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=24)
+
+caseName = basins.wrapMaster(
+    'basinRef', 'first80', batchSize=[None, 500], optQ=1, outName='basinRef-opt1')
 cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/cmdTrain.py -M'
 slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=24)
