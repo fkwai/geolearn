d7a0801e151650885d8e666f84bf763f3084c33e
Kuai Fang <geofkwai@gmail.com>
2020-04-10 14:48:24 -0400

    fix a minor bug
    should not harm

--- app/waterQual/model/basinRef_subset.py
+++ app/waterQual/model/basinRef_subset.py
@@ -13,6 +13,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 wqData = waterQuality.DataModelWQ('basinRef')
+
+# rmnan in q
+q = wqData.q[:, :, 0]
+info = wqData.info
+len(np.where(np.isnan(q).all(axis=0))[0])
+len(np.where(np.isnan(q).any(axis=0))[0])
+len(wqData.info)  # nan in Q - 3% all nan, 7% any nan
+indR = np.where(np.isnan(q).any(axis=0))[0]
+indK = np.where(~np.isnan(q).any(axis=0))[0]
+# purify q
+infoK = info.iloc[indK]
+dfSite = waterQuality.countSite(infoK)
+ind1 = dfSite[dfSite['pRank'] <= 0.5].index.values
+ind2 = dfSite[dfSite['pRank'] > 0.5].index.values
+wqData.saveSubset(['pQ-F50', 'pQ-L50'], [ind1, ind2])
+# yr after purify q
+indYr = waterQuality.indYr(infoK)
+yrLst = ['Y80', 'Y90', 'Y00', 'Y10']
+wqData.saveSubset(['pQ-'+x for x in yrLst], indYr)
+indYrCmp = list()
+indAll = infoK.index.values
+for ind in indYr:
+    indYrCmp.append(np.setdiff1d(indAll, ind))
+wqData.saveSubset(['pQ-rm'+x for x in yrLst], indYrCmp)
+# validate
+# d=wqData.info.iloc[wqData.subset['pQ-Y00']]['date']
+# np.sort(pd.DatetimeIndex(d).year.unique())
+
+# regional / area subsets
 varG = ['DRAIN_SQKM', 'ECO2_BAS_DOM', 'NUTR_BAS_DOM',
         'HLR_BAS_DOM_100M']
 tabG = gageII.readData(varLst=varG, siteNoLst=wqData.siteNoLst)
@@ -20,7 +49,6 @@ info = wqData.info
 indF50 = wqData.indByRatio(0.5)
 indL50 = wqData.indByRatio(0.5, first=False)
 wqData.saveSubset(['first50', 'last50'], [indF50, indL50])
-
 
 siteNoLst = list()
 nameLst = list()
