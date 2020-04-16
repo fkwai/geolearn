1467316630ef8fd86415fe626aa0ced08721bcf6
Kuai Fang <geofkwai@gmail.com>
2020-04-16 13:22:33 -0400

    subset hbn

--- app/waterQual/model/HBN_single.py
+++ app/waterQual/model/HBN_single.py
@@ -10,7 +10,6 @@ import os
 import json
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 wqData = waterQuality.DataModelWQ('HBN')
 figFolder = os.path.join(kPath.dirWQ, 'HBN')
@@ -20,14 +19,22 @@ doLst.append('subset')
 
 if 'subset' in doLst:
     # find ind have SiO4, P
-    codeLst = ['00618', '00950'，'00681']
-    ic = [wqData.varC.index(code) for code in codeLst]
-    indAll = np.where(~np.isnan(wqData.c[:, ic]).all(axis=1))[0]
+    codeLst = ['00618', '00955']
+    icLst = [wqData.varC.index(code) for code in codeLst]
+    indAll = np.where(~np.isnan(wqData.c[:, icLst]).all(axis=1))[0]
+    indAny = np.where(~np.isnan(wqData.c[:, icLst]).any(axis=1))[0]
+    # print number of samples
+    for code in codeLst:
+        ic = wqData.varC.index(code)
+        indC = np.where(~np.isnan(wqData.c[:, ic]))[0]
+        print(code, len(indC))
     # seperate index by years
-    indYr = waterQuality.indYr(wqData.info.iloc[indAll], yrLst=[2010, 2020])[0]
-    indYrCmp = np.setdiff1d(indAll, indYr)
-    wqData.saveSubset('-'.join(sorted(codeLst))+'-Y10', indYr)
-    wqData.saveSubset('-'.join(sorted(codeLst))+'-rmY10', indYrCmp)
+    for ind, lab in zip([indAll, indAny], ['all', 'any']):
+        indYr = waterQuality.indYr(
+            wqData.info.iloc[ind], yrLst=[2010, 2020])[0]
+        indYrCmp = np.setdiff1d(ind, indYr)
+    wqData.saveSubset('-'.join(sorted(codeLst)+[lab,'Y10']), indYr)
+    wqData.saveSubset('-'.join(sorted(codeLst)+[lab,'rmY10']), indYrCmp)
     for code in codeLst:
         ic = wqData.varC.index(code)
         indC = np.where(~np.isnan(wqData.c[:, ic]))[0]
@@ -36,8 +43,7 @@ if 'subset' in doLst:
         indYrCmp = np.setdiff1d(indC, indYr)
         wqData.saveSubset(code+'-Y10', indYr)
         wqData.saveSubset(code+'-rmY10', indYrCmp)
-    # d=wqData.info.iloc[wqData.subset['00665-Y10']]['date']
+    # d=wqData.info.iloc[wqData.subset['00618-00955-any-Y10']]['date']
     # np.sort(pd.DatetimeIndex(d).year.unique())
-    # ind=wqData.info.iloc[wqData.subset['00665-Y10']].index.values
-    # wqData.c[ind, wqData.varC.index('00665')]
-
+    # ind=wqData.info.iloc[wqData.subset['00618-00955-any-Y10']].index.values
+    # wqData.c[ind, wqData.varC.index('00618')]
