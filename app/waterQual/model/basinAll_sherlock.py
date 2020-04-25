36f11cdf62791934ce450e81c00ae67982374477
fkwai <geofkwai@gmail.com>
2020-04-25 08:49:40 -0700

    sh

--- app/waterQual/model/basinAll_sherlock.py
+++ app/waterQual/model/basinAll_sherlock.py
@@ -1,18 +1,32 @@
 from hydroDL.master import basins
 from hydroDL.app import waterQuality
 from hydroDL.master import slurm
+from hydroDL.data import gageII, usgs, gridMET
 
 # wqData = waterQuality.DataModelWQ('basinAll')
 # ind1 = wqData.indByRatio(0.8)
 # ind2 = wqData.indByRatio(0.8, first=False)
 # wqData.saveSubset(['first80', 'last20'], [ind1, ind2])
+
+# devide to 8090 and 0010
+wqData = waterQuality.DataModelWQ('basinAll')
+indYr1 = waterQuality.indYr(
+    wqData.info, yrLst=[1979, 2000])[0]
+wqData.saveSubset('Y8090', indYr1)
+indYr2 = waterQuality.indYr(
+    wqData.info, yrLst=[2000, 2020])[0]
+wqData.saveSubset('Y0010', indYr2)
+
+
+caseLst = list()
+subsetLst = ['Y8090', 'Y0010']
+for subset in subsetLst:
+    saveName = 'basinAll-{}-opt1'.format(subset)
+    caseName = basins.wrapMaster(dataName='basinAll', trainName=subset, saveEpoch=50,
+                                 batchSize=[None, 2000], outName=saveName)
+    caseLst.append(caseName)
+
+
 cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/cmdTrain.py -M {}'
-
-
-caseName = basins.wrapMaster(
-    'basinAll', 'first80', batchSize=[None, 2000], optQ=1, outName='basinAll-opt1')
-slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=48,nM=64)
-
-caseName = basins.wrapMaster(
-    'basinAll', 'first80', batchSize=[None, 2000], optQ=2, outName='basinAll-opt2')
-slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=48,nM=64)
+for caseName in caseLst:
+    slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=48, nM=64)
---
+++ app/waterQual/simple/sherlock.py
@@ -0,0 +1,39 @@
+from hydroDL.master import slurm
+from hydroDL import kPath
+from hydroDL.app import waterQuality
+from hydroDL.data import gageII, usgs, gridMET
+from hydroDL.master import basins
+
+import pandas as pd
+import numpy as np
+import os
+import time
+
+
+caseLst = list()
+dataName = 'Silica64Mess'
+subsetLst = ['Y8090', 'Y0010']
+codeLst = ['00955']
+for subset in subsetLst:
+    saveName = '{}-{}-opt1'.format(dataName, subset)
+    caseName = basins.wrapMaster(dataName=dataName, trainName=subset, 
+                                 batchSize=[None, 200], outName=saveName)
+    caseLst.append(caseName)
+    saveName = '{}-{}-opt2'.format(dataName, subset)
+    caseName = basins.wrapMaster(dataName=dataName, trainName=subset,
+                                 batchSize=[None, 200], varY=None, 
+                                 varX=usgs.varQ+gridMET.varLst, outName=saveName)
+    caseLst.append(caseName)
+    # saveName = '{}-{}-opt3'.format(dataName, subset)
+    # caseName = basins.wrapMaster(dataName=dataName, trainName=subset,
+    #                              batchSize=[None, 200], varY=None, outName=saveName)
+    # caseLst.append(caseName)
+    # saveName = '{}-{}-opt4'.format(dataName, subset)
+    # caseName = basins.wrapMaster(dataName=dataName, trainName=subset,
+    #                              batchSize=[None, 200], varYC=None, outName=saveName)
+    # caseLst.append(caseName)
+
+
+cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/cmdTrain.py -M {}'
+for caseName in caseLst:
+    slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=12)
