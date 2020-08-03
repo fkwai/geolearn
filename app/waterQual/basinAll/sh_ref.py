b99c6eb4ddce8b31f806e877dee0ec767ebf1ba3
fkwai <geofkwai@gmail.com>
2020-08-02 23:35:14 -0700

    sh

--- app/waterQual/basinAll/sh_ref.py
+++ app/waterQual/basinAll/sh_ref.py
@@ -11,21 +11,25 @@ import time
 
 
 caseLst = list()
-# dataName = 'basinRef'
-dataName = 'stanRef'
+dataName = 'basinRef'
+# dataName = 'basinRef-q'
 subsetLst = ['Yodd', 'Yeven']
 for subset in subsetLst:
-    # saveName = '{}-{}-opt1'.format(dataName, subset)
-    # caseName = basins.wrapMaster(dataName=dataName, trainName=subset,
-    #                              batchSize=[None, 200], outName=saveName)
-    # caseLst.append(caseName)
+    saveName = '{}-{}-opt1'.format(dataName, subset)
+    caseName = basins.wrapMaster(dataName=dataName, trainName=subset,
+                                 batchSize=[None, 200], outName=saveName)
+    caseLst.append(caseName)
     saveName = '{}-{}-opt2'.format(dataName, subset)
     caseName = basins.wrapMaster(dataName=dataName, trainName=subset,
                                  batchSize=[None, 200], varY=None,
                                  varX=usgs.varQ+gridMET.varLst, outName=saveName)
     caseLst.append(caseName)
+    # saveName = '{}-{}'.format(dataName, subset)
+    # caseName = basins.wrapMaster(dataName=dataName, trainName=subset, varYC=usgs.varC+['00060'],
+    #                              batchSize=[None, 200], outName=saveName)
+    # caseLst.append(caseName)
 
 
 cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/app/waterQual/model/cmdTrain.py -M {}'
 for caseName in caseLst:
-    slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=30)
+    slurm.submitJobGPU(caseName, cmdP.format(caseName), nH=24)
