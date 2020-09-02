260a1db6120fd033e3b5c584db67f61df9bd2049
Kuai Fang <geofkwai@gmail.com>
2020-09-02 16:27:02 -0700

    ntn reading
    update

---
+++ app/data/ntn/testResample.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = pd.date_range(start='2019-12-31', end='2020-02-01', freq='D')
+v = np.arange(len(t))
+
+df = pd.DataFrame(index=t, columns=['v'], data=v)
+
+df.resample('W-TUE').mean()
+
+
+# offset = pd.offsets.timedelta(days=-6)
+# df.resample('W-MON', loffset=offset).mean()
