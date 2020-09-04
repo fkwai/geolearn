f1a0fd82c02b8308ef5780883554b905c9758c74
Kuai Fang <geofkwai@gmail.com>
2020-09-03 20:00:52 -0700

    move to legacy
    lots of stuff to legacy

--- app/data/ntn/testResample.py
+++ app/data/ntn/testResample.py
@@ -4,13 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-t = pd.date_range(start='2019-12-31', end='2020-02-01', freq='D')
-v = np.arange(len(t))
+t = pd.date_range(start='2019-12-31', end='2020-02-1', freq='D')
+v = np.arange(len(t)).astype(float)
+v[3:16] = np.nan
 
 df = pd.DataFrame(index=t, columns=['v'], data=v)
+
+df.interpolate(limit=5, limit_direction='both', limit_area='inside')
 
 df.resample('W-TUE').mean()
 
 
 # offset = pd.offsets.timedelta(days=-6)
 # df.resample('W-MON', loffset=offset).mean()
+
+
+df.v.isnull().astype(int).groupby(df.v.notnull().astype(
+    int).cumsum()).cumsum()
+
+dfI = df.isnull().astype(int)
+dfI.cumsum()
