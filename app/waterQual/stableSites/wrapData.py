be010cdfa40e51539c4b7e9079737c1e4108ba3d
Kuai Fang <geofkwai@gmail.com>
2020-09-06 09:06:33 -0700

    temp for sh

--- app/waterQual/stableSites/wrapData.py
+++ app/waterQual/stableSites/wrapData.py
@@ -13,7 +13,8 @@ with open(os.path.join(dirSel, 'dictSB_0412.json')) as f:
     dictSite = json.load(f)
 siteNoLst = dictSite['comb0']
 
-freqLst = ['W', 'D']
+# freqLst = ['W', 'D']
+freqLst = ['D']
 optCLst = ['end', 'seq']
 for freq in freqLst:
     for optC, optLabel in zip(optCLst, ['', 'T']):
@@ -22,18 +23,3 @@ for freq in freqLst:
         print(dataName)
         wqData = waterQuality.DataModelWQ.new(
             dataName, siteNoLst, rho=rho, freq=freq, optC=optC)
-
-
-# # create subset
-# for code in dictSite.keys():
-#     print(code)
-#     siteNoLst = dictSite[code]
-#     b1 = wqData.info['siteNo'].isin(siteNoLst).values
-#     if code != 'comb':
-#         b2 = ~np.isnan(wqData.c[-1, :, wqData.varC.index(code)])
-#         info = wqData.info[b1 & b2]
-#     else:
-#         info = wqData.info[b1]
-#     indY1, indY2 = waterQuality.indYrOddEven(info)
-#     wqData.saveSubset('{}-Y1'.format(code), indY1)
-#     wqData.saveSubset('{}-Y2'.format(code), indY2)
