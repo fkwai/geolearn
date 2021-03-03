4b5041618b68a6cc0ae7068c7a04d4dc3ab07ad1
Kuai Fang <geofkwai@gmail.com>
2021-03-03 00:19:42 -0800

    Update sh_Y30N5.py

--- app/wqFull/sh_Y30N5.py
+++ app/wqFull/sh_Y30N5.py
@@ -8,15 +8,15 @@ varX = gridMET.varLst
 varY = ['runoff']+usgs.newC
 varXC = gageII.lstWaterQuality
 varYC = None
-dataName = 'sbTest'
+dataName = 'sbY30N5'
 
 sd = '1979-01-01'
 ed = '2010-01-01'
 
 outName = '{}-B10'.format(dataName)
 master = basinFull.wrapMaster(outName=outName, dataName=dataName, varX=varX,
-                                varY=varY, varXC=varXC, varYC=varYC,
-                                sd=sd, ed=ed)
+                              varY=varY, varXC=varXC, varYC=varYC,
+                              sd=sd, ed=ed)
 
 cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/hydroDL/master/cmd/basinFull.py -M {}'
 slurm.submitJobGPU(outName, cmdP.format(outName), nH=24, nM=32)
