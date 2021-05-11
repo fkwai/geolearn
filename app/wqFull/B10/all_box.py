8579d3c80add23a5375fabefd40b5b18ba116b26
Kuai Fang <geofkwai@gmail.com>
2021-05-10 20:03:19 -0400

    update

--- app/wqFull/B10/all_box.py
+++ app/wqFull/B10/all_box.py
@@ -19,16 +19,46 @@ labelLst = ['QFPRT2C', 'QFPT2C', 'FPRT2QC', 'QT2C']
 rhoLst = [365, 10]
 
 dataName = 'brDN5'
-label = 'QFPRT2C'
+labelLst = ['QFPRT2C', 'QFPT2C']
 rho = 365
-outName = '{}-{}-t{}-B10'.format(dataName, label, rho)
-dm = dbBasin.DataModelFull(dataName)
-master = basinFull.loadMaster(outName)
-varY = master['varY']
-testSet = 'all'
-sd = '1982-01-01'
-ed = '2018-12-31'
+for k, label in enumerate(labelLst):
+    outName = '{}-{}-t{}-B10'.format(dataName, label, rho)
+    dm = dbBasin.DataModelFull(dataName)
+    master = basinFull.loadMaster(outName)
+    varY = master['varY']
+    testSet = 'all'
+    sd = '1982-01-01'
+    ed = '2018-12-31'
+    ns = len(dm.siteNoLst)
+    nc = len(varY)
+    yP, ycP = basinFull.testModel(
+        outName, DM=dm, batchSize=20, testSet=testSet, ep=100)
+    yO, ycO = basinFull.getObs(outName, testSet, DM=dm)
 
-yP, ycP = basinFull.testModel(
-    outName, DM=dm, batchSize=20, testSet=testSet, ep=100)
-yO, ycO = basinFull.getObs(outName, testSet, DM=dm)
+    corrMat = np.ndarray([ns, nc, 2])
+    for ic in range(nc):
+        indT = np.where(dm.t == np.datetime64('2010-01-01'))[0][0]
+        corr1 = utils.stat.calCorr(yP[:indT, :, ic], yO[:indT, :, ic])
+        corr2 = utils.stat.calCorr(yP[indT:, :, ic], yO[indT:, :, ic])
+        corrMat[:, ic, k] = corr2
+
+dataBox = list()
+for ic in range(nc):
+    temp = [corrMat[:, ic, 0], corrMat[:, ic, 1]]
+    # temp = [rmseMat[:, ic, 0], rmseMat[:, ic, 1]]
+    dataBox.append(temp)
+
+labLst1 = ['{}\n{}'.format(usgs.codePdf.loc[code]
+                           ['shortName'], code) for code in varY]
+labLst2 = ['w/o LAI', 'w/ LAI']
+fig = figplot.boxPlot(dataBox, widths=0.5, cLst='br', label1=labLst1, label2=labLst2,
+                      figsize=(6, 4), yRange=[0, 1])
+fig.show()
+
+# ts random
+iP = np.random.randint(ns)
+code = '00915'
+iC = varY.index(code)
+fig, ax = plt.subplots(1, 1)
+axplot.plotTS(ax, dm.t, [yP[:, iP, iC], yO[:, iP, iC]], styLst='-*', cLst='bk')
+fig.show()
