7ddd87c10aa13316a384eabf305b3ed1c6e29c0e
Kuai Fang <geofkwai@gmail.com>
2021-06-17 00:57:58 -0700

    Update shNorm.py

--- app/wqFull/sherlock/shNorm.py
+++ app/wqFull/sherlock/shNorm.py
@@ -26,18 +26,4 @@ for dataName in dataNameLst:
                                  nEpoch=500, batchSize=[365, 1000],
                                  mtdX=mtdX, mtdY=mtdY, mtdXC=mtdXC, mtdYC=mtdYC)
     cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/hydroDL/master/cmd/basinFull.py -M {}'
-    # slurm.submitJobGPU(outName, cmdP.format(outName), nH=24, nM=32)
-    basinFull.trainModel(outName)
-
-    DF = dbBasin.DataFrameBasin(dictP['dataName'])
-    dictVar = {k: dictP[k]
-               for k in ('varX', 'varXC', 'varY', 'varYC')}
-    DM = dbBasin.DataModelBasin(DF, subset=dictP['trainSet'], **dictVar)
-    dataTup = DM.getDataRaw()
-    [nx, nxc, ny, nyc, nt, ns] = trainBasin.getSize(dataTup)
-    [rho, nbatch] = [365, 1000]
-    matB = ~np.isnan(dataTup[2][rho:, :, :])
-    nD = np.sum(np.any(matB, axis=2))
-    pr = nbatch*rho/ns/nt*nD/ns/nt
-    int(np.ceil(np.log(0.01) / np.log(1 - pr)))
-
+    slurm.submitJobGPU(outName, cmdP.format(outName), nH=24, nM=32)
