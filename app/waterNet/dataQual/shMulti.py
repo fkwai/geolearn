b0c4b4974f5419d7b368627ca1b444859396e6dc
Kuai Fang <geofkwai@gmail.com>
2021-09-12 23:42:30 -0700

    sh

--- app/waterNet/dataQual/shMulti.py
+++ app/waterNet/dataQual/shMulti.py
@@ -1,3 +1,4 @@
+import os
 from hydroDL.master import slurm
 from hydroDL.master import basinFull
 from hydroDL.data import gageII, dbBasin
@@ -10,6 +11,7 @@ codeLst = ['00600', '00618', '00915', '00945', '00955']
 pLst = [100, 75, 50, 25]
 nyLst = [6, 8, 10]
 
+outLst = list()
 for code in codeLst:
     for ny in nyLst:
         for p in pLst:
@@ -24,10 +26,18 @@ for code in codeLst:
             varYC = None
             mtdYC = [code]
             outName = '{}-{}-{}-{}'.format(dataName, label, trainSet, code)
+            outFolder = basinFull.nameFolder(outName)
+            modelFile = os.path.join(outFolder, 'modelState_ep100')
             dictP = basinFull.wrapMaster(outName=outName, dataName=dataName, trainSet=trainSet,
                                          nEpoch=500, batchSize=[365, 20], nIterEp=50,
                                          varX=varX, varY=varY, varXC=varXC, varYC=varYC,
                                          mtdX=mtdX, mtdY=mtdY, mtdXC=mtdXC, mtdYC=mtdYC)
-            cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/hydroDL/master/cmd/basinFull.py -M {}'
-            slurm.submitJobGPU(outName, cmdP.format(outName))
-            # basinFull.trainModel(outName)
+            outFolder = basinFull.nameFolder(outName)
+            modelFile = os.path.join(outFolder, 'modelState_ep100')
+            if not os.path.exists(modelFile):
+                outLst.append(outName)
+print(outLst)
+for outName in outLst:
+    cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/hydroDL/master/cmd/basinFull.py -M {}'
+    # slurm.submitJobGPU(outName, cmdP.format(outName))
+    # basinFull.trainModel(outName)
