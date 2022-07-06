c57e55d51eed596a94040347a8ce28d5dd7d30f2
Kuai Fang <geofkwai@gmail.com>
2022-07-05 21:16:33 -0700

    sh hyper

---
+++ app/wqLSTM/paper/src/shN200-hs.py
@@ -0,0 +1,34 @@
+from hydroDL.data import usgs, gageII, gridMET, ntn, GLASS
+from hydroDL.master import slurm
+from hydroDL.data import dbBasin
+from hydroDL.master import basinFull
+
+dataName = 'N200'
+label = 'QFPRT2C'
+# DF = dbBasin.DataFrameBasin(dataName)
+rho = 1000
+nbatch = 500
+hs = 256
+trainSet = 'rmYr5'
+testSet = 'pkYr5'
+
+hsLst = [16, 32, 64, 128, 512]
+for hs in hsLst:
+    varX = dbBasin.label2var(label.split('2')[0])
+    mtdX = dbBasin.io.extractVarMtd(varX)
+    varY = dbBasin.label2var(label.split('2')[1])
+    mtdY = dbBasin.io.extractVarMtd(varY)
+    varXC = gageII.varLst
+    mtdXC = dbBasin.io.extractVarMtd(varXC)
+    varYC = None
+    mtdYC = dbBasin.io.extractVarMtd(varYC)
+    outName = '{}-{}-{}-hs{}'.format(dataName, label, trainSet, hs)
+    dictP = basinFull.wrapMaster(
+        outName=outName, dataName=dataName, trainSet=trainSet,
+        nEpoch=500, saveEpoch=50,
+        varX=varX, varY=varY, varXC=varXC, varYC=varYC,
+        mtdX=mtdX, mtdY=mtdY, mtdXC=mtdXC, mtdYC=mtdYC,
+        hiddenSize=hs, batchSize=[rho, nbatch])
+    cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/hydroDL/master/cmd/basinFull.py -M {}'
+    slurm.submitJobGPU(outName, cmdP.format(outName), nH=24, nM=64)
+# basinFull.trainModel(outName)
