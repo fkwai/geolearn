546e064d306a9b9b4d52769e0cee42b0d82f33c7
fkwai <geofkwai@gmail.com>
2023-01-25 11:08:20 -0800

    update sh

--- app/data/usgsSample/wrapAll.py
+++ app/data/usgsSample/wrapAll.py
@@ -1,20 +1,20 @@
 
-from hydroDL.data import dbBasin, usgs, gageII
-from hydroDL.post import axplot, figplot, mapplot
-import matplotlib.pyplot as plt
+from hydroDL.data import dbBasin, usgs, gageII, gridMET
 import os
 from hydroDL import kPath
 import numpy as np
 import pandas as pd
 
-# not work due to memory issue
-fileSiteNo = os.path.join(kPath.dirData, 'USGS', 'inventory', 'siteNoLst-1979')
-siteNoLst = pd.read_csv(fileSiteNo, header=None, dtype=str)[0].tolist()
 
-dataName = 'dbAll_82_18'
+siteNoFile=os.path.join(kPath.dirUsgs, 'basins', 'siteCONUS.csv')
+dfSite=pd.read_csv(siteNoFile,dtype={'siteNo':str})
+siteNoLst=dfSite['siteNo'].tolist()
+
+dataName = 'dbAll'
 codeLst = usgs.varC+usgs.codeIso
 DF = dbBasin.DataFrameBasin.new(
-    dataName, siteNoLst, varC=codeLst, varG=gageII.varLstEx)
+    dataName, siteNoLst, varC=codeLst, varF = gridMET.varLst,
+    varG=gageII.varLstEx,edStr='2022-01-01')
 DF.saveSubset('WYB09', sd='1982-01-01', ed='2009-10-01')
 DF.saveSubset('WYA09', sd='2009-10-01', ed='2018-12-31')
 DF = dbBasin.DataFrameBasin(dataName)
