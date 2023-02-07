a5f97c23420cee27da4ee88264b755b78e002b54
Kuai Fang <geofkwai@gmail.com>
2023-02-07 15:35:10 -0800

    sh

--- app/wqLSTM/NY5/shTrain.py
+++ app/wqLSTM/NY5/shTrain.py
@@ -43,5 +43,5 @@ for label in labelLst:
             nIterEp=2
         )
         cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/hydroDL/master/cmd/basinFull.py -M {}'
-        # slurm.submitJobGPU(outName, cmdP.format(outName), nH=24, nM=64)
-        basinFull.trainModel(outName)
+        slurm.submitJobGPU(outName, cmdP.format(outName), nH=24, nM=64)
+        # basinFull.trainModel(outName)
