de946fa52f6701d41ad8cc9eace16be09f6385d1
Kuai Fang <geofkwai@gmail.com>
2023-02-27 15:40:40 -0800

    sh

--- app/wqLSTM/B200/shWRTDS.py
+++ app/wqLSTM/B200/shWRTDS.py
@@ -3,7 +3,7 @@ from hydroDL.master import slurm
 dataName = 'rmTK-B200'
 # trainLst = ['B15']+['rmYr5b{}'.format(k) for k in range(5)]+['rmRT5b{}'.format(k) for k in range(5)]
 
-trainLst = 'rmYr5b0'
+trainLst = ['rmYr5b0']
 
 for trainSet in trainLst:
     cmdP = 'python /home/users/kuaifang/GitHUB/geolearn/hydroDL/master/cmd/WRTDS.py -D {} -T {}'
