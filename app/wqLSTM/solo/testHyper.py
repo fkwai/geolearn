f4bdc1a62dc22594829a26a6374b61a014d7f21a
fkwai <geofkwai@gmail.com>
2023-03-21 12:50:40 -0700

    hyper param

--- app/wqLSTM/solo/testHyper.py
+++ app/wqLSTM/solo/testHyper.py
@@ -9,13 +9,17 @@ import os
 from hydroDL.master import basinFull
 from hydroDL.master import slurm
 
+code = '00915'
+dataName = '{}-{}'.format(code, 'B200')
+DF = dbBasin.DataFrameBasin(dataName)
+
+
 def testModel(code, dr, hs, rho, nLayer,ep=500):
-        dataName = '{}-{}'.format(code, 'B200')
         outName = '{}-{}-{}-d{:.0f}-h{}-rho{}-nl{}'.format(
             dataName, label, trainSet, dr * 100, hs, rho, nLayer
         )
-        yP1, ycP1 = basinFull.testModel(outName, testSet=trainSet, ep=ep)
-        yP2, ycP2 = basinFull.testModel(outName, testSet=testSet, ep=ep)
+        yP1, ycP1 = basinFull.testModel(outName, DF=DF,testSet=trainSet, ep=ep)
+        yP2, ycP2 = basinFull.testModel(outName, DF=DF,testSet=testSet, ep=ep)
         return outName
 
 drLst = [0.25, 0.5, 0.75]
@@ -25,7 +29,6 @@ nLayerLst = [1, 2]
 
 # codeLst = ['00618','00915','00955']
 
-code = '00915'
 # for label in labelLst:
 label = 'QFT2C'
 trainSet = 'rmYr5b0'
@@ -36,8 +39,8 @@ for dr in drLst:
             for nLayer in nLayerLst:
                 for ep in [100,300, 500]:
                     try:
-                        outName=testModel(code, dr, hs, rho, nLayer)
+                        outName=testModel(code, dr, hs, rho, nLayer,ep=ep)
                     except:
                         print(code, dr, hs, rho, nLayer)
 
-
+print('finished')
